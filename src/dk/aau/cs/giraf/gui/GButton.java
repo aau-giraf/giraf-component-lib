3fba5d90a3218263ad01a0d9916b4168d563d31d
Benjamin Hubert <bhuber11@student.aau.dk>
2014-03-07 09:00:45 +0100

    The new and better GButton. Support for images that are automatically scaled to fit. Bad performance because the scaling is in onDraw(). Automatic gradient calculation as well.

--- src/dk/aau/cs/giraf/gui/GButton.java
+++ src/dk/aau/cs/giraf/gui/GButton.java
@@ -4,7 +4,11 @@
 package dk.aau.cs.giraf.gui;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Matrix;
+import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.graphics.drawable.GradientDrawable;
 import android.util.AttributeSet;
@@ -18,15 +22,31 @@ import android.widget.Toast;
 public class GButton extends Button {
 
     private static final float shadingMultiplier = 1.2f;
+    private Drawable buttonImage;
+    private int test = 0;
 
+
+    @Override
+    public void setCompoundDrawablesWithIntrinsicBounds(Drawable left, Drawable top, Drawable right, Drawable bottom)
+    {
+        if (left != null) buttonImage = left;
+        else if (top != null) buttonImage = top;
+        else if (right != null) buttonImage = right;
+        else if (bottom != null) buttonImage = bottom;
+    }
+
+    public void onDraw(Canvas c)
+    {
+        super.onDraw(c);
+        buttonImage = resizeIcon(buttonImage, this.getHeight() - this.getPaddingBottom() - this.getPaddingTop());
+        super.setCompoundDrawablesWithIntrinsicBounds(buttonImage, null, null, null);
+    }
     /**
      * Styles the button according to the giraf standards.
      * Theme support pending.
      */
-	private void setStyle(AttributeSet attrs) {
+	private void setStyle() {
 		this.setBackgroundResource(R.drawable.gbutton);
-
-        String test = attrs.getAttributeValue("http://schemas.android.com/apk/res/android", "drawableLeft");
 
         //default colors
         this.setTextColor(Color.parseColor("#9E6435"));
@@ -48,7 +68,6 @@ public class GButton extends Button {
                 red & 0xFF0000 |
                 green & 0xFF00 |
                 blue;
-        this.setText(test);
 
         int[] colors = new int[2];
         colors[0] = colorStart;
@@ -57,6 +76,21 @@ public class GButton extends Button {
         GradientDrawable gd = new GradientDrawable(GradientDrawable.Orientation.TOP_BOTTOM, colors);
         this.setBackgroundDrawable(gd);
 	}
+
+    private Drawable resizeIcon(Drawable img, int height){
+        Drawable result = img;
+        Bitmap tempIcon = ((BitmapDrawable)result).getBitmap();
+
+        final int oldWidth = tempIcon.getWidth();
+        final int oldHeight = tempIcon.getHeight();
+
+        float scale = ((float) height) / ((float)tempIcon.getHeight());
+
+        tempIcon = Bitmap.createScaledBitmap(tempIcon, (int)(tempIcon.getWidth()*scale), (int)(tempIcon.getHeight()*scale), true);
+        result = new BitmapDrawable(tempIcon);
+
+        return result;
+    }
 	
 	/**
 	 * @param context
@@ -64,7 +98,7 @@ public class GButton extends Button {
 	public GButton(Context context) {
 		super(context);
 		// TODO Auto-generated constructor
-		//this.setStyle();
+		this.setStyle();
 	}
 
 	/**
@@ -74,7 +108,8 @@ public class GButton extends Button {
 	public GButton(Context context, AttributeSet attrs) {
 		super(context, attrs);
 		// TODO Auto-generated constructor stub
-		this.setStyle(attrs);
+		this.setStyle();
+        //thatID = attrs.getAttributeIntValue("http://schemas.android.com/apk/res/android", "drawableLeft", 0);
 
 	}
 
@@ -86,7 +121,7 @@ public class GButton extends Button {
 	public GButton(Context context, AttributeSet attrs, int defStyle) {
 		super(context, attrs, defStyle);
 		// TODO Auto-generated constructor stub
-		this.setStyle(attrs);
+		this.setStyle();
 	}
 
 }
