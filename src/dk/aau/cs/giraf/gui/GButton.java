909a87df170c6a56123e215f5a90b9adfae65d44
Malakahh <michael_fuglsang@hotmail.com>
2014-03-07 09:50:59 +0100

    Modified GButton, added GStyler class (for containing utility functions such as scaleDrawable()).

--- src/dk/aau/cs/giraf/gui/GButton.java
+++ src/dk/aau/cs/giraf/gui/GButton.java
@@ -4,42 +4,45 @@
 package dk.aau.cs.giraf.gui;
 
 import android.content.Context;
-import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
-import android.graphics.Matrix;
-import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.graphics.drawable.GradientDrawable;
 import android.util.AttributeSet;
 import android.widget.Button;
-import android.widget.Toast;
 
-/**
- * @author thomaskobberpanum aka asshole
- *
- */
 public class GButton extends Button {
 
-    private static final float shadingMultiplier = 1.2f;
+    //private static final float shadingMultiplier = 1.2f;
     private Drawable buttonImage;
-    private int test = 0;
-
 
     @Override
     public void setCompoundDrawablesWithIntrinsicBounds(Drawable left, Drawable top, Drawable right, Drawable bottom)
     {
+        //Idea of method:
+        //          Avoid setting the image in this method, only store it and scale it. Actual setting is done in onDraw().
+        //Reason:
+        //          Default XML behavior undesirable, we needed to scale the image before it is sat, to avoid button
+        //          scaling when sat to wrap-content.
+
+        //Save drawable for future use
         if (left != null) buttonImage = left;
         else if (top != null) buttonImage = top;
         else if (right != null) buttonImage = right;
         else if (bottom != null) buttonImage = bottom;
+
+        //Scales drawable to match size of button
+        if (buttonImage != null)
+            buttonImage = GStyler.scaleDrawable(buttonImage, this.getHeight() - this.getPaddingBottom() - this.getPaddingTop());
     }
 
     public void onDraw(Canvas c)
     {
         super.onDraw(c);
-        buttonImage = resizeIcon(buttonImage, this.getHeight() - this.getPaddingBottom() - this.getPaddingTop());
-        super.setCompoundDrawablesWithIntrinsicBounds(buttonImage, null, null, null);
+
+        //Sets the button image.
+        if (buttonImage != null)
+            super.setCompoundDrawablesWithIntrinsicBounds(buttonImage, null, null, null);
     }
     /**
      * Styles the button according to the giraf standards.
@@ -52,6 +55,7 @@ public class GButton extends Button {
         this.setTextColor(Color.parseColor("#9E6435"));
         int colorStart = Color.parseColor("#FFFFD96E");
 
+/*
         //collect the RGB values from the hex color code via bit-shifting
         int red = colorStart & (0xFF << 16);
         int green = colorStart & (0xFF << 8);
@@ -68,29 +72,14 @@ public class GButton extends Button {
                 red & 0xFF0000 |
                 green & 0xFF00 |
                 blue;
-
+*/
         int[] colors = new int[2];
         colors[0] = colorStart;
-        colors[1] = colorEnd;
+        colors[1] = GStyler.calculateGradientColor(colorStart);
 
         GradientDrawable gd = new GradientDrawable(GradientDrawable.Orientation.TOP_BOTTOM, colors);
         this.setBackgroundDrawable(gd);
 	}
-
-    private Drawable resizeIcon(Drawable img, int height){
-        Drawable result = img;
-        Bitmap tempIcon = ((BitmapDrawable)result).getBitmap();
-
-        final int oldWidth = tempIcon.getWidth();
-        final int oldHeight = tempIcon.getHeight();
-
-        float scale = ((float) height) / ((float)tempIcon.getHeight());
-
-        tempIcon = Bitmap.createScaledBitmap(tempIcon, (int)(tempIcon.getWidth()*scale), (int)(tempIcon.getHeight()*scale), true);
-        result = new BitmapDrawable(tempIcon);
-
-        return result;
-    }
 	
 	/**
 	 * @param context
