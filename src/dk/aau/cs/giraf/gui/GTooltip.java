a0c9b140802b49040b7406cb81b350e1b1f245d3
Dennis Nielsen <dennis@nobullshitgames.com>
2014-03-17 13:09:58 +0100

    Adding tooltip, crashes may occur

--- src/dk/aau/cs/giraf/gui/GTooltip.java
+++ src/dk/aau/cs/giraf/gui/GTooltip.java
@@ -5,8 +5,10 @@ import android.content.Context;
 import android.util.TypedValue;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.RelativeLayout.LayoutParams;
+import android.widget.TextView;
 
 public class GTooltip extends Dialog {
 	
@@ -14,14 +16,27 @@ public class GTooltip extends Dialog {
 	private Context mContext;
 	private final int padding = 5;
 	private ViewGroup mViewGroup;
+    private String text;
+    private int color;
+    private TextView tv;
 
-	public GTooltip(Context context) {
+	public GTooltip(Context context, String text, int color) {
 		super(context, android.R.style.Theme_Translucent_NoTitleBar);
 		mContext = context;
 		this.setStyle();
 		mTip = this;
+
+        this.text = text;
+        this.color = color;
+
+        this.tv = new TextView(context);
+        tv.setText(text);
+        tv.setTextColor(color);
+
+        mTip.setView(tv);
+
 		this.findViewById(R.id.tooltip_hitarea).setOnClickListener(new View.OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
 				mTip.cancel();
@@ -47,19 +62,26 @@ public class GTooltip extends Dialog {
 		this.setContentView(R.layout.gtooltip_layout);
 		mViewGroup = (ViewGroup) this.findViewById(R.id.tooltip_content);
 	}
-	
+
+
 	public void setRightOf(View v) {
 		View target = this.findViewById(R.id.gtooltip_arrow_view);
-		
-		int parentMarginLeft = ((RelativeLayout.LayoutParams) ((ViewGroup) v.getParent()).getLayoutParams()).leftMargin;
-		int newLeft = intToDP(v.getLeft() + v.getWidth() + padding) + parentMarginLeft;
-		int newTop = intToDP(v.getTop() + (v.getHeight()/2) - 12); 
-	
-		android.widget.RelativeLayout.LayoutParams p = (android.widget.RelativeLayout.LayoutParams) target.getLayoutParams();
-		p.setMargins(newLeft, newTop, 0, 0);
-		target.setLayoutParams(p);
-		
 
+        int[] pos = new int[2];
+        v.getLocationOnScreen(pos);
+
+        pos[0] += v.getMeasuredWidth() +0.5f;
+        pos[1] += v.getMeasuredHeight()/2 + mViewGroup.getMeasuredHeight()/2;
+
+        tv.setText(String.valueOf(target.getBottom()));
+
+        target.setX(pos[0]);
+        target.setY(pos[1]);
+
+        //this.tv.setText(String.valueOf(v.getHeight()) + "-" + String.valueOf(pos[1]));
+
+        mViewGroup.setX(pos[0]);
+        mViewGroup.setY(pos[1]);
 	}
 	
 	private int intToDP(int i) {
