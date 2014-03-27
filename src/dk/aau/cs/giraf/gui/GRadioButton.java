d580d50b2ddfc7fa3a8a7b6aa662caf15f9e88b3
Benjamin Hubert <bhuber11@student.aau.dk>
2014-03-27 14:08:50 +0100

    Added GRadioButton and GRadioGroup. Currently they are not different from their android counterparts.

---
+++ src/dk/aau/cs/giraf/gui/GRadioButton.java
@@ -0,0 +1,26 @@
+package dk.aau.cs.giraf.gui;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.RadioButton;
+
+/**
+ * Created by Jelly on 27-03-14.
+ */
+public class GRadioButton extends RadioButton {
+
+    public GRadioButton(Context context)
+    {
+        super(context);
+    }
+
+    public GRadioButton(Context context, AttributeSet attrs)
+    {
+        super(context, attrs);
+    }
+
+    public GRadioButton(Context context, AttributeSet attrs, int defStyle)
+    {
+        super(context, attrs, defStyle);
+    }
+}
