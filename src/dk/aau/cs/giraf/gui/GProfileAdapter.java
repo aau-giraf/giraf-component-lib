334ae042314e50ecc85d96c739526b58afddd8db
Anders Bender <abende11@student.aau.dk>
2014-04-08 15:57:26 +0200

    Fixed TextView taking focus from GList

--- src/dk/aau/cs/giraf/gui/GProfileAdapter.java
+++ src/dk/aau/cs/giraf/gui/GProfileAdapter.java
@@ -2,19 +2,13 @@ package dk.aau.cs.giraf.gui;
 
 import android.app.Activity;
 import android.content.Context;
-import android.graphics.Color;
-import android.graphics.drawable.Drawable;
 import android.graphics.drawable.GradientDrawable;
-import android.graphics.drawable.LayerDrawable;
 import android.graphics.drawable.StateListDrawable;
-import android.graphics.drawable.shapes.Shape;
 import android.util.StateSet;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
-import android.widget.LinearLayout;
-import android.widget.TextView;
 
 import java.util.List;
 
@@ -25,6 +19,9 @@ public class GProfileAdapter extends BaseAdapter {
 	private Activity activity;
     private List<Profile> data;
     private static LayoutInflater inflater=null;
+    protected GradientDrawable stylePressed;
+    protected GradientDrawable styleUnPressed;
+    protected GradientDrawable picBackground;
     
     public GProfileAdapter(Activity a, List<Profile> d) {
         activity = a;
@@ -44,10 +41,6 @@ public class GProfileAdapter extends BaseAdapter {
         return position;
     }
 
-    protected GradientDrawable stylePressed;
-    protected GradientDrawable styleUnPressed;
-    protected GradientDrawable picBackground;
-
     public View getView(int position, View convertView, ViewGroup parent) {
         View vi = convertView;
         if(convertView==null)
@@ -63,27 +56,6 @@ public class GProfileAdapter extends BaseAdapter {
         colorsPressed[0] = colors[1];
         colorsPressed[1] = GStyler.calculateGradientColor(colorsPressed[0]);
 
-        //make the two gradients
-        styleUnPressed = new GradientDrawable(GradientDrawable.Orientation.TOP_BOTTOM, colors);
-        stylePressed = new GradientDrawable(GradientDrawable.Orientation.TOP_BOTTOM, colorsPressed);
-
-        //round corners and give edges
-        styleUnPressed.setCornerRadius(10);
-        styleUnPressed.setStroke(3, GStyler.calculateGradientColor(colors[0], 0.75f));
-        stylePressed.setCornerRadius(10);
-        stylePressed.setStroke(3, GStyler.calculateGradientColor(colorsPressed[0], 0.75f));
-
-        //set state_pressed to gdPressed and all others to gd
-        stateListDrawable.addState(new int[] {android.R.attr.state_pressed}, stylePressed);
-        stateListDrawable.addState(new int[] {android.R.attr.state_pressed, android.R.attr.state_selected}, stylePressed);
-        stateListDrawable.addState(StateSet.WILD_CARD, styleUnPressed);
-
-        vi.setPadding(GStyler.dpToPixel(10, vi.getContext())
-                , GStyler.dpToPixel(5, vi.getContext())
-                , GStyler.dpToPixel(10, vi.getContext())
-                , GStyler.dpToPixel(5, vi.getContext()));
-
-        vi.setBackgroundDrawable(stateListDrawable);
         GTextView name = (GTextView)vi.findViewById(R.id.profile_name); // title
 //        ImageView thumb_image=(ImageView)vi.findViewById(R.id.list_image); // thumb image
 
@@ -102,13 +74,30 @@ public class GProfileAdapter extends BaseAdapter {
         profileBackground.setBackgroundDrawable(picBackground);
         // Setting all values in listview
         name.setText(profile.getName());
+        //make the two gradients
+        styleUnPressed = new GradientDrawable(GradientDrawable.Orientation.TOP_BOTTOM, colors);
+        stylePressed = new GradientDrawable(GradientDrawable.Orientation.TOP_BOTTOM, colorsPressed);
+
+        //round corners and give edges
+        styleUnPressed.setCornerRadius(10);
+        styleUnPressed.setStroke(3, GStyler.calculateGradientColor(colors[0], 0.75f));
+        stylePressed.setCornerRadius(10);
+        stylePressed.setStroke(3, GStyler.calculateGradientColor(colorsPressed[0], 0.75f));
+
+        //set state_pressed to gdPressed and all others to gd
+        stateListDrawable.addState(new int[] {android.R.attr.state_selected, android.R.attr.state_pressed}, stylePressed);
+        stateListDrawable.addState(new int[] {android.R.attr.state_selected}, stylePressed);
+        stateListDrawable.addState(new int[] {android.R.attr.state_pressed}, stylePressed);
+        stateListDrawable.addState(new int[] {-android.R.attr.state_pressed}, styleUnPressed);
+
+        vi.setPadding(GStyler.dpToPixel(10, vi.getContext())
+                , GStyler.dpToPixel(5, vi.getContext())
+                , GStyler.dpToPixel(10, vi.getContext())
+                , GStyler.dpToPixel(5, vi.getContext()));
+
+        vi.setBackgroundDrawable(stateListDrawable);
         //imageLoader.DisplayImage(song.get(CustomizedListView.KEY_THUMB_URL), thumb_image);
         return vi;
-    }
-
-    private void setStyle()
-    {
-
     }
 
 }
