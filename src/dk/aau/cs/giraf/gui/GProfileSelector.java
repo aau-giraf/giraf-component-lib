d22a31841a0ddfbcec02d03aa39dc437550404da
Anders Bender <abende11@student.aau.dk>
2014-04-08 16:08:56 +0200

    GProfileSelector Listener added

--- src/dk/aau/cs/giraf/gui/GProfileSelector.java
+++ src/dk/aau/cs/giraf/gui/GProfileSelector.java
@@ -2,14 +2,10 @@ package dk.aau.cs.giraf.gui;
 
 import android.app.Activity;
 import android.content.Context;
-import android.content.res.Resources;
-import android.graphics.Color;
-import android.graphics.drawable.BitmapDrawable;
-import android.graphics.drawable.Drawable;
 import android.graphics.drawable.GradientDrawable;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.LinearLayout;
 
 import java.util.ArrayList;
@@ -23,32 +19,49 @@ import dk.aau.cs.giraf.oasis.lib.models.Profile;
 public class GProfileSelector extends GDialog {
 
     protected GradientDrawable picBackground;
+    protected GList theList;
 
     public GProfileSelector(Context context, List<Profile> profileList, Profile currentProfile)
     {
         super(context);
+        //Inflate Views
+        View completeView = LayoutInflater.from(this.getContext()).inflate(R.layout.gprofile_selector, null);
+        View currentProfileView = LayoutInflater.from(this.getContext()).inflate(R.layout.gprofile_row, null);
 
-        View w = LayoutInflater.from(this.getContext()).inflate(R.layout.gprofile_selector, null);
-        View o = LayoutInflater.from(this.getContext()).inflate(R.layout.gprofile_row, null);
-        GList theList = (GList) w.findViewById(R.id.profileSelectorList);
-        LinearLayout i =(LinearLayout) w.findViewById(R.id.profileSelectorButton);
-        LinearLayout s = (LinearLayout) o.findViewById(R.id.profile_pic);
+        //Find the different components
+        theList = (GList) completeView.findViewById(R.id.profileSelectorList);
+        LinearLayout currentProfileLayer =(LinearLayout) completeView.findViewById(R.id.currentProfileLayer);
+        LinearLayout profilePicture = (LinearLayout) currentProfileView.findViewById(R.id.profile_pic);
+        GTextView currentPersonTextView = (GTextView) currentProfileView.findViewById(R.id.profile_name);
+        currentPersonTextView.setText(currentProfile.getName());
+
+        //Create a back dynamic background to the profile picture accordingly to GStyler
         picBackground = new GradientDrawable(GradientDrawable.Orientation.TOP_BOTTOM,
                          new int[] {GStyler.calculateGradientColor(GStyler.calculateGradientColor(GStyler.buttonBaseColor)),GStyler.calculateGradientColor(GStyler.calculateGradientColor(GStyler.buttonBaseColor))});
         picBackground.setCornerRadius(5);
-        s.setPadding(GStyler.dpToPixel(3, w.getContext())
-                , GStyler.dpToPixel(3, w.getContext())
-                , GStyler.dpToPixel(3, w.getContext())
-                , GStyler.dpToPixel(3, w.getContext()));
+        profilePicture.setPadding(GStyler.dpToPixel(3, completeView.getContext())
+                , GStyler.dpToPixel(3, completeView.getContext())
+                , GStyler.dpToPixel(3, completeView.getContext())
+                , GStyler.dpToPixel(3, completeView.getContext()));
+        profilePicture.setBackgroundDrawable(picBackground);
 
-        s.setBackgroundDrawable(picBackground);
-        i.addView(o);
+        //Add the inflated currentProfileView to the currentProfileLayer
+        currentProfileLayer.addView(currentProfileView);
 
+       //create and set the adapter to the list
         GProfileAdapter profileAdapter = new GProfileAdapter((Activity) context, profileList);
-
         theList.setAdapter(profileAdapter);
 
+        //Set the completeview to the Dialog
+        this.SetView(completeView);
+    }
 
-        this.SetView(w);
+    /**
+     * Used to set the OnItemClickListener to the List from outside the Dialog
+     * @param listener The Listener desired for the List's OnItemClickListener
+     */
+    public void setOnListItemClick(AdapterView.OnItemClickListener listener)
+    {
+        theList.setOnItemClickListener(listener);
     }
 }
