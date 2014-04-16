c2dd39061e763877e184217c2a8ab9eeddeba1d7
Benjamin Hubert <bhuber11@student.aau.dk>
2014-04-16 12:45:21 +0200

    Fixed GToggleButton

--- src/dk/aau/cs/giraf/gui/GToggleButton.java
+++ src/dk/aau/cs/giraf/gui/GToggleButton.java
@@ -2,25 +2,30 @@ package dk.aau.cs.giraf.gui;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
+import android.widget.ToggleButton;
 
 /**
  * Created by Jelly on 25-03-14.
  */
 public class GToggleButton extends GButton {
 
-    private boolean state = false;
+    private boolean toggled = false;
+    final GToggleButton btn;
 
     public GToggleButton(Context context)
     {
         super(context);
+        btn = this;
         setOnClickListener(null);
     }
 
     public GToggleButton(Context context, AttributeSet attrs)
     {
         super(context, attrs);
+        btn = this;
         if (attrs.getAttributeValue("http://schemas.android.com/apk/res/android", "onClick") == null)
             setOnClickListener(null);
     }
@@ -28,6 +33,7 @@ public class GToggleButton extends GButton {
     public GToggleButton(Context context, AttributeSet attrs, int defStyle)
     {
         super(context, attrs, defStyle);
+        btn = this;
         if (attrs.getAttributeValue("http://schemas.android.com/apk/res/android", "onClick") == null)
             setOnClickListener(null);
     }
@@ -38,32 +44,27 @@ public class GToggleButton extends GButton {
         super.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
-                state = !state;
-                if (task != null) task.onClick(v);
-                if (state)
-                    v.setBackgroundDrawable(((GButton) v).stylePressed);
-                else
-                    v.setBackgroundDrawable(((GButton) v).styleUnPressed);
+                btn.setToggled(!btn.isToggled());
+                if (task != null)
+                    task.onClick(v);
             }
         });
     }
 
-    @Override
-    public boolean isPressed()
+    public boolean isToggled()
     {
-        return state;
+        return toggled;
+
     }
 
-    public void setPressed(boolean pressed)
+
+    public void setToggled(boolean state)
     {
-        state = pressed;
-        
-        if (state)
+        toggled = state;
+
+        if (isToggled())
             this.setBackgroundDrawable(stylePressed);
         else
             this.setBackgroundDrawable(styleUnPressed);
     }
-
-
-
 }
