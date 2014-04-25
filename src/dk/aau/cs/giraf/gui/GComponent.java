c92bf7b291557bc130b0726a63153bd6c9f03746
Dennis Nielsen <dennis@nobullshitgames.com>
2014-04-25 10:04:23 +0200

    Testing out gradient return value

--- src/dk/aau/cs/giraf/gui/GComponent.java
+++ src/dk/aau/cs/giraf/gui/GComponent.java
@@ -1,6 +1,8 @@
 package dk.aau.cs.giraf.gui;
 
 import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.GradientDrawable;
 
 import java.util.HashMap;
 
@@ -9,6 +11,7 @@ import dk.aau.cs.giraf.oasis.lib.models.Setting;
 
 public class GComponent {
 
+    private ColorificationBisimulationRelation c = new ColorificationBisimulationRelation(GStyler.baseColor);
 
     public static void SetBaseColor(int color)
     {
@@ -22,6 +25,11 @@ public class GComponent {
     public static int DpToPixel(int dp, Context context)
     {
         return GStyler.dpToPixel(dp, context);
+    }
+
+    public static GradientDrawable GetBackgroundGradient()
+    {
+        return new GradientDrawable(GradientDrawable.Orientation.BOTTOM_TOP, new int[] { GStyler.baseColor, GStyler.InversePropoertionallyAlterVS(GStyler.baseColor, 0.75f) });
     }
 
 }
