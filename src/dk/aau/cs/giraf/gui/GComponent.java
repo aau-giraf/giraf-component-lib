aea2e35a60aa7e96d71c7d9ea552ae2f6df30e7c
Dennis Nielsen <dennis@nobullshitgames.com>
2014-04-29 11:23:40 +0200

    Testint out new background shizzle

--- src/dk/aau/cs/giraf/gui/GComponent.java
+++ src/dk/aau/cs/giraf/gui/GComponent.java
@@ -1,6 +1,10 @@
 package dk.aau.cs.giraf.gui;
 
 import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.GradientDrawable;
 
 import java.util.HashMap;
 
@@ -9,6 +13,11 @@ import dk.aau.cs.giraf.oasis.lib.models.Setting;
 
 public class GComponent {
 
+    public enum Background{
+        SOLID,
+        GRADIENT,
+        SUBTLEGRADIENT
+    }
 
     public static void SetBaseColor(int color)
     {
@@ -17,6 +26,21 @@ public class GComponent {
 
     public static int GetBackgroundColor(){
         return GStyler.backgroundColor;
+    }
+
+    public static Drawable GetBackground(Background type)
+    {
+        switch (type){
+            case SOLID:
+                return new ColorDrawable(GStyler.backgroundColor);
+            case GRADIENT:
+                return new GradientDrawable(GradientDrawable.Orientation.BOTTOM_TOP, new int[] {
+                        Color.parseColor("#FFffe062"),
+                        Color.parseColor("#FFebbc39")
+                });
+            default:
+                return new ColorDrawable(GStyler.backgroundColor);
+        }
     }
 
     public static int DpToPixel(int dp, Context context)
