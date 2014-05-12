4314bdb2d3a671a1a6857fac68788be851010366
Benjamin Hubert <bhuber11@student.aau.dk>
2014-05-12 15:02:02 +0200

    Added GEditText

--- src/dk/aau/cs/giraf/gui/GEditText.java
+++ src/dk/aau/cs/giraf/gui/GEditText.java
@@ -1,7 +1,32 @@
 package dk.aau.cs.giraf.gui;
 
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.EditText;
+
 /**
  * Created by Jelly on 12-05-14.
  */
-public class GEditText {
+public class GEditText extends EditText {
+
+
+    public GEditText(Context context) {
+        super(context);
+        SetStyle();
+    }
+
+    public GEditText(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        SetStyle();
+    }
+
+    public GEditText(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        SetStyle();
+    }
+
+    private void SetStyle()
+    {
+        this.setTextColor(GStyler.textBaseColor);
+    }
 }
