91a0e12f6f6a00f9b7ebecf1974b2ba8b4f4c1ae
marhlder <marhlder@gmail.com>
2015-03-10 13:10:05 +0100

    Added GViewPager (Read: Giraf View Pager)

--- src/main/java/dk/aau/cs/giraf/gui/GViewPager.java
+++ src/main/java/dk/aau/cs/giraf/gui/GViewPager.java
@@ -1,7 +1,30 @@
 package dk.aau.cs.giraf.gui;
 
+import android.content.Context;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
+import android.util.AttributeSet;
+
 /**
  * Created by Marhlder on 10-03-2015.
  */
-public class GViewPager {
+public class GViewPager extends android.support.v4.view.ViewPager {
+
+    public GViewPager(Context context) {
+        super(context);
+        init();
+    }
+
+    public GViewPager(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    private void init()
+    {
+        int glowDrawableId = getContext().getResources().getIdentifier("overscroll_glow", "drawable", "android");
+        Drawable androidGlow = getContext().getResources().getDrawable(glowDrawableId);
+        androidGlow.setColorFilter(getResources().getColor(R.color.giraf_fading_edge_glow), PorterDuff.Mode.MULTIPLY);
+    }
+
 }
