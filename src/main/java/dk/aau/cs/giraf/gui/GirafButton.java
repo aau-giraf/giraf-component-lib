f0570fbddb604504da8369a1a30b9c61b764b428
Rasmus Holm Jensen <rhje12@student.aau.dk>
2015-03-20 09:33:52 +0100

    Update GirafButton

--- src/main/java/dk/aau/cs/giraf/gui/GirafButton.java
+++ src/main/java/dk/aau/cs/giraf/gui/GirafButton.java
@@ -25,24 +25,56 @@ import dk.aau.cs.giraf.utilities.GirafScalingUtilities;
  */
 public class GirafButton extends LinearLayout {
 
+    // The inner views of the GirafButton
     private ImageView iconView;
+    private AutoResizeTextView textView;
+
+    // The content of the inner view of the GirafButton
     private Drawable icon;
+    private String buttonText;
 
     // Sets the max width and height of the button
     private final int ICON_MAX_WIDTH = (int) GirafScalingUtilities.convertDpToPixel(this.getContext(), 45);
     private final int ICON_MAX_HEIGHT = (int) GirafScalingUtilities.convertDpToPixel(this.getContext(), 45);
 
+    /**
+     * Constructs an {@link dk.aau.cs.giraf.gui.GirafButton} without xml
+     * @param context the context
+     * @param icon the drawable icon of the {@link dk.aau.cs.giraf.gui.GirafButton}
+     */
     public GirafButton(Context context, Drawable icon) {
+        this(context, icon, null);
+    }
+
+    /**
+     * Constructs an {@link dk.aau.cs.giraf.gui.GirafButton} without xml and text
+     * @param context the context
+     * @param icon the drawable icon of the {@link dk.aau.cs.giraf.gui.GirafButton}
+     * @param buttonText the text of the {@link dk.aau.cs.giraf.gui.GirafButton}
+     */
+    public GirafButton(Context context, Drawable icon, String buttonText) {
         super(context);
         this.icon = icon; // Set the icon to the property
+        this.buttonText = buttonText; // Set the text to the property
         initializeButton(null); // Use the dynamic icon from code
     }
 
+    /**
+     *  Construct an {@link dk.aau.cs.giraf.gui.GirafButton} from xml
+     * @param context the context
+     * @param attrs the attributes from the xml-declaration
+     */
     public GirafButton(Context context, AttributeSet attrs) {
         super(context, attrs);
         initializeButton(attrs);
     }
 
+    /**
+     *  Constructs an {@link dk.aau.cs.giraf.gui.GirafButton} from xml
+     * @param context the context
+     * @param attrs the attributes from the xml-declaration
+     * @param defStyle the style
+     */
     public GirafButton(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
         initializeButton(attrs);
@@ -74,12 +106,28 @@ public class GirafButton extends LinearLayout {
             TypedArray girafButtonAttributes = getContext().obtainStyledAttributes(attrs, R.styleable.GirafButton);
 
             // Find the attributes from the instance of the GirafButton
-            icon = girafButtonAttributes.getDrawable(R.styleable.GirafButton_icon); // Finds the icon
+            icon = girafButtonAttributes.getDrawable(R.styleable.GirafButton_icon); // Finds the icon from xml
+
+            // If the text was not set from the constructor
+            if(buttonText == null)
+            {
+                buttonText = girafButtonAttributes.getString(R.styleable.GirafButton_text); // Finds the text from xml
+            }
+
         } else {
             icon = this.icon;
         }
 
         iconView.setImageDrawable(icon); // Sets the icon into the ImageView
+
+        // Initialize the textView
+        textView = new AutoResizeTextView(this.getContext());
+
+        // If the text was set in the xml attributes or the constructor
+        if(buttonText != null)
+        {
+            textView.setText(buttonText); // Set
+        }
 
         // Set the background of the button (with all sates)
         this.setBackgroundResource(R.drawable.giraf_button_background);
@@ -92,6 +140,9 @@ public class GirafButton extends LinearLayout {
 
         // Add the icon of the button
         this.addView(iconView, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.MATCH_PARENT));
+
+        // Add the text of the button
+        this.addView(textView);
     }
 
 }
