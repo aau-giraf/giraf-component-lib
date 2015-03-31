42262435213a4ed86be9ab80827e5d5e992b5324
Rasmus Holm Jensen <rhje12@student.aau.dk>
2015-03-31 15:19:58 +0200

    Add GirafSpinner

--- src/main/java/dk/aau/cs/giraf/gui/GirafSpinner.java
+++ src/main/java/dk/aau/cs/giraf/gui/GirafSpinner.java
@@ -2,15 +2,31 @@ package dk.aau.cs.giraf.gui;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.util.TypedValue;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.SpinnerAdapter;
+import android.widget.TextView;
+
+import dk.aau.cs.giraf.utilities.GirafScalingUtilities;
 
 /**
- * Created by rasmusholmjensen on 26/03/15.
+ * Created on 26/03/15.
  */
 public class GirafSpinner extends Spinner {
 
     public GirafSpinner(Context context, AttributeSet attrs) {
         super(context, attrs);
+        initializeSpinner(attrs);
     }
+
+    private void initializeSpinner(AttributeSet attrs) {
+        this.setBackgroundResource(R.drawable.giraf_button_background);
+        this.setPadding(0,0,0,0);
+        this.setGravity(Gravity.CENTER);
+    }
+
 }
--- src/main/java/dk/aau/cs/giraf/gui/GirafSpinnerAdapter.java
+++ src/main/java/dk/aau/cs/giraf/gui/GirafSpinnerAdapter.java
@@ -1,9 +1,11 @@
 package dk.aau.cs.giraf.gui;
 
 import android.content.Context;
+import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.util.List;
@@ -13,26 +15,31 @@ import java.util.List;
  */
 public class GirafSpinnerAdapter<T> extends ArrayAdapter<T> {
 
-    public GirafSpinnerAdapter(Context context, List<T> items) {
+    public GirafSpinnerAdapter(final Context context, final List<T> items) {
         super(context, R.layout.giraf_spinner_item, items);
     }
 
     @Override
     public final View getView(final int position, final View convertView, final ViewGroup parent) {
         final TextView view = (TextView) super.getView(position, convertView, parent);
-
         view.setText(getItemName(getItem(position)));
-
+        view.setGravity(Gravity.CENTER);
         return view;
+    }
+
+    @Override
+    public View getDropDownView(final int position, final View convertView, final ViewGroup parent) {
+        return super.getView(position, convertView, parent);
     }
 
     /**
      * This method should be overridden, to return the name of the item, if item.toString is not the desired name of the item.
+     *
      * @param item item in the list
      * @return the name of this item
      */
-    public String getItemName(final T item)
-    {
+    public String getItemName(final T item) {
         return item.toString();
     }
+
 }
