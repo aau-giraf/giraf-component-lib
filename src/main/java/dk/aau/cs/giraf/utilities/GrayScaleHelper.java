9a88b9b8f54a57430e3abd3c4178f33ae1605988
Christoffer Mourtizen <cmouri14@student.aau.dk>
2017-04-24 13:06:09 +0200

    Added better grayscale method which only needs an activity and a boolean to make view and action bar gray

--- src/main/java/dk/aau/cs/giraf/utilities/GrayScaleHelper.java
+++ src/main/java/dk/aau/cs/giraf/utilities/GrayScaleHelper.java
@@ -1,16 +1,29 @@
 package dk.aau.cs.giraf.utilities;
 
+import android.app.Activity;
 import android.content.Context;
 import android.graphics.ColorMatrix;
 import android.graphics.ColorMatrixColorFilter;
 import android.graphics.Paint;
 import android.view.View;
+import android.view.Window;
+import dk.aau.cs.giraf.gui.R;
 
 import static android.view.View.LAYER_TYPE_HARDWARE;
 import static android.view.View.LAYER_TYPE_NONE;
 
 public class GrayScaleHelper {
-    public static void setGray(View view, boolean state){
+
+    public static void setGrayScaleForActivity(Activity activity, boolean shouldBeGray){
+        View screen = activity.getWindow().getDecorView();
+        int barId = activity.getResources().getIdentifier("action_bar_container", "id", "android");
+        View bar = screen.findViewById(barId);
+        View view = activity.findViewById(android.R.id.content);
+        setGray(view,shouldBeGray);
+        setGray(bar,shouldBeGray);
+    }
+
+    private static void setGray(View view, boolean state){
         if(state) {
             ColorMatrix cm = new ColorMatrix();
             cm.setSaturation(0); //Set the color saturation to 0
